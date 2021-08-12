07c9aceabbb585c885e461212f851f8c1f23e4df
Rajan Christian <15846532+thecybermafia@users.noreply.github.com>
2021-08-12 17:10:11 +0530

    Update email_request.go

--- models/email_request.go
+++ models/email_request.go
@@ -7,7 +7,7 @@ import (
 	"net/mail"
 	"strings"
 
-	"github.com/gophish/gomail"
+	"github.com/thecybermafia/gomail"
 	"github.com/gophish/gophish/config"
 	log "github.com/gophish/gophish/logger"
 	"github.com/gophish/gophish/mailer"
